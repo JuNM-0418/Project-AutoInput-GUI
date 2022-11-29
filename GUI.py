d6ec20e848223bcbcf6e75f0573b85fea5d3f5c7
JuNM-0418 <goojsm13@naver.com>
2022-11-29 17:44:14 +0900

    Update AutoIniputGUI

--- GUI.py
+++ GUI.py
@@ -1,139 +1,174 @@
-import MAIN as M
+import MAIN as Document
 import tkinter as tk
 import tkinter.messagebox
 import tkinter.ttk as ttk
-import win32com.client as win32
+from tkinter import filedialog
 import time
 
 
-def inputImage():
+def selectFile():
+    excelFilePath = filedialog.askopenfilename(initialdir='./',title='파일선택', filetypes=(('xlsx files','*.xlsx'),('all files','*.*')))
+    Start.setExcelFilePath(excelFilePath) 
+    Start.setWorkBook()
+    Start.setWorkSheetsName()
+    buttonSelectFile.config(state="disabled")
+   
+    
 
-    buttonStart.config(state="disabled")
+def selectSheet():
+    buttonSelectSheet.config(state="disabled")
 
-    Start.setFileName(entryFileName.get())
-
-    SheetsName = str(entrySheetsName.get())
-    SheetsName = SheetsName.replace(" ","")
-    SheetsNameList = SheetsName.split(',')
-
-
-    BuildingName = str(entryBuildingName.get())
-    BuildingName = BuildingName.replace(" ","")
-    BuildingNameList = BuildingName.split(',')
+    listbox = tk.Listbox(root, selectmode='extended', height=5)
+    for sheet in enumerate(Start.getWorkSheetsName()):
+        listbox.insert(sheet[0], sheet[1])
+    listbox.pack()
 
 
-    WorkState = True
+    def selectComplete():
+        selectedSheetList = listbox.curselection()
+
+        for selectedSheet in selectedSheetList:
+            Start.setSheetsNameList(Start.getWorkSheetsName()[selectedSheet])
+
+        listbox.delete(0, len(Start.getWorkSheetsName()))
+        for index in range(len(selectedSheetList)):
+            listbox.insert(index, Start.getSheetsNameList()[index])
+
+        listbox.config(state="disabled")
+        buttonComplete.config(state="disabled")
+        createImgPathButton()
+
+    buttonComplete = tk.Button(root, text="선택완료",command = selectComplete)
+    buttonComplete.pack()
+    tkinter.ttk.Separator(root, orient="horizontal").pack(fill="both")
 
 
-    for SheetsName, BuildingName in zip(SheetsNameList, BuildingNameList):
-        try:
-            Start.setSheetsName(SheetsName)
-            Start.setBuildingName(BuildingName)
 
-            Start.setWorkBook()
-            Start.setWorkSheets()
-            Start.setFileList()
-            Start.setFileListJpg()
-            Start.setImageNum()
-            Start.setPageNum()
-            Start.setLastPageNum()
+def createImgPathButton():
+    index = 1
+    for size in range(len(Start.getSheetsNameList())):
+        labelListSelectImgPath.append(tk.Label(root, text=f"사진 폴더{index}를 선택하세요."))
+        labelListSelectImgPath[size].pack()
+        buttonListSelectImgPath.append(tk.Button(root, text="열기", command=selectImgFilePath))
+        buttonListSelectImgPath[size].pack()
+        index+=1
 
-            Start.duplicateImage()
+    
+    
+    
+def selectImgFilePath():
+    global check
+    check+=1
+    imgFilePath = filedialog.askdirectory(initialdir="./",title='폴더 선택')
+    Start.setImgFilePathList(imgFilePath)
+    
 
-            PageNum = int(Start.getPageNum())
-            
-            WorkBarState = tk.DoubleVar()
-            WorkBar = ttk.Progressbar(root,maximum=PageNum,variable=WorkBarState)   
-            WorkBar.pack() 
+    if (check == len(Start.getSheetsNameList())):
+        for index in range(len(Start.getSheetsNameList())):
+            buttonListSelectImgPath[index].config(state="disabled")
+        tkinter.ttk.Separator(root, orient="horizontal").pack(fill="both")
 
-        except:
-            tkinter.messagebox.showerror("에러 발생", "엑셀 파일, 시트 또는 폴더 이름을 확인해주세요.")
-            break
+        buttonInsertImg.pack()
 
-        for i in range(0, int(PageNum), 1):
+
+def inputImg():
+    buttonInsertImg.config(state="disabled")
+    Start.setImgFileList()
+    Start.setImageNum()
+    Start.setPageNum()
+    Start.setLastPageNum()
+    Start.setWorkSheets()
+    Start.duplicateImage()
+    
+
+    
+   
+
+
+    for index, data in enumerate(Start.getPageNum()):
+
+        WorkState = True
+        WorkBarState = tk.DoubleVar()
+        WorkBar = ttk.Progressbar(root,maximum=len(Start.getPageNum()),variable=WorkBarState)   
+        WorkBar.pack()
+
+        for i in range(0, data, 1):
             time.sleep(0.01)
             WorkBarState.set(i+1)
             WorkBar.update()
-            
-            try:
-                # 사진, 화살표 삽입 및 조사표 내용 삽입
-                Start.imageCycle = Start.inputImage(Start.locationRowTop,Start.location_Col_1)
-                Start.contentsCycle = Start.inputContents(Start.locationRowTop)
-                Start.inputArrow(Start.locationRowTop)
-                Start.inputLocation(Start.locationRowTop)
-                
-
-                Start.imageCycle = Start.inputImage(Start.locationRowMid,Start.location_Col_1)
-                Start.contentsCycle = Start.inputContents(Start.locationRowMid)
-                Start.inputArrow(Start.locationRowMid)
-                Start.inputLocation(Start.locationRowMid)
-
-                Start.imageCycle = Start.inputImage(Start.locationRowBottom,Start.location_Col_1)
-                Start.contentsCycle = Start.inputContents(Start.locationRowBottom)
-                Start.inputArrow(Start.locationRowBottom)
-                Start.inputLocation(Start.locationRowBottom)
-
-            except Exception as e:
-                WorkState = False
-                tkinter.messagebox.showerror("에러 발생", str(Start.BuildingName) + " " + str(Start.imageCycle) + ".jpg 사진이 없습니다.")
-                print(e)
-                break
 
             
-                
-            Start.combineExplanation(Start.locationRowTop,Start.location_Col_2)
-            Start.combineExplanation(Start.locationRowMid,Start.location_Col_2)
-            Start.combineExplanation(Start.locationRowBottom,Start.location_Col_2)
-            Start.checkImageNum(Start.locationRowTop)
-            Start.checkImageNum(Start.locationRowMid)
-            Start.checkImageNum(Start.locationRowBottom)
+            Start.inputImage(Start.locationRowTop, Start.location_Col_1, index)
+            Start.inputContents(Start.locationRowTop, index)
+            Start.inputArrow(Start.locationRowTop, index)
+            Start.inputLocation(Start.locationRowTop, index)
+
+            Start.inputImage(Start.locationRowMid,Start.location_Col_1, index)
+            Start.inputContents(Start.locationRowMid, index)
+            Start.inputArrow(Start.locationRowMid, index)
+            Start.inputLocation(Start.locationRowMid, index)
+
+            Start.inputImage(Start.locationRowBottom,Start.location_Col_1, index)
+            Start.inputContents(Start.locationRowBottom, index)
+            Start.inputArrow(Start.locationRowBottom, index)
+            Start.inputLocation(Start.locationRowBottom, index)
+
+            Start.combineExplanation(Start.locationRowTop,Start.location_Col_2, index)
+            Start.combineExplanation(Start.locationRowMid,Start.location_Col_2, index)
+            Start.combineExplanation(Start.locationRowBottom,Start.location_Col_2, index)
+
+            Start.checkImageNum(Start.locationRowTop, index)
+            Start.checkImageNum(Start.locationRowMid, index)
+            Start.checkImageNum(Start.locationRowBottom, index)
 
             Start.locationRowTop = Start.nextLocation(Start.locationRowTop)
             Start.locationRowMid = Start.nextLocation(Start.locationRowMid)
             Start.locationRowBottom = Start.nextLocation(Start.locationRowBottom)
-            
-        if(WorkState == False) :
-            break
 
 
+        
+ 
         Start.locationRowTop = 3 # 페이지 내 첫번째 시작 행
         Start.locationRowMid = 13 # 페이지 내 두번쨰 시작 행
         Start.locationRowBottom = 23 # 페이지 내 세번째 시작 행
-        Start.location_Col_1 = "B" # 고정 열
-        Start.location_Col_2 = "U" # 번호확인 함수 위치
-        Start.location_Col_3 = "O" # 설명확인 함수 위치
         Start.imageCycle = 1   # 각 동의 폴더에서 넣을 사진의 순서
-        Start.contentsCycle =  1 # 조사표 내용이 삽입되는 순서
+        Start.contentCycle =  1 # 조사표 내용이 삽입되는 순서
 
-    Start.excel.Visible=True
     tkinter.messagebox.showinfo("작업 완료", "모든 사진이 삽입 되었습니다.")
     root.destroy()
-    
+    Start.excel.Visible=True    
 
-Start = M.Main()
+
+
+Start = Document.Info()
+
 root = tk.Tk()
 root.title("안전점검")
-root.geometry('300x300')
+root.geometry('300x800')
+
+global check
+check = 0
 
 
-labelFileName = tk.Label(root, text = "파일 이름을 입력해주세요.")
-labelFileName.pack()
-entryFileName = tk.Entry(root)
-entryFileName.pack()
+excelFilePath = None
+labelImgPath = []
+labelListSelectImgPath = []
+buttonListSelectImgPath = []
 
-labelSheetsName = tk.Label(root,text="시트 이름을 입력해주세요.")
-labelSheetsName.pack()
-entrySheetsName = tk.Entry(root)
-entrySheetsName.pack()
+labelSelectFile = tk.Label(root, text = "파일을 선택하세요.")
+labelSelectFile.pack()
+buttonSelectFile = tk.Button(root, text="열기", command=selectFile)
+buttonSelectFile.pack()
+
+tkinter.ttk.Separator(root, orient="horizontal").pack(fill="both")
 
 
-labelBuildingName = tk.Label(root, text="동 이름을 입력해주세요.")
-labelBuildingName.pack()
-entryBuildingName = tk.Entry(root)
-entryBuildingName.pack()
+labelSelectSheet = tk.Label(root,text="시트를 선택하세요.")
+labelSelectSheet.pack()
+buttonSelectSheet = tk.Button(root, text="열기", command=selectSheet)
+buttonSelectSheet.pack()
 
-buttonStart = tk.Button(root,text="입력 완료", command=inputImage)
-buttonStart.pack()
 
+buttonInsertImg = tk.Button(root, text="삽입 시작", command=inputImg)
 
 root.mainloop()
--- MAIN.py
+++ MAIN.py
@@ -3,16 +3,21 @@ import os
 import shutil
 
 
-class Main: 
+class Info: 
     
-    fileName = None
-    buildingNum = 0
-    sheetsName = None
-    buildingName = None
-    path = os.getcwd()
+    excelFilePath = None
+    imgFilePathList = []
+    sheetsNameList = []
+    imgFileList = []
+    imageNum = []  # .jpg로 끝나는 사진의 개수
+    pageNum = [] # 사진이 무조건 다 들어가는 페이지수
+    lastPageNum = [] # 마지막 페이지에서 모자란 사진 숫자
+
+   
     excel = win32.gencache.EnsureDispatch('Excel.Application')
     wb = None
-    ws = None
+    ws = []
+    wsName = None
     locationRowTop = 3 # 페이지 내 첫번째 시작 행
     locationRowMid = 13 # 페이지 내 두번쨰 시작 행
     locationRowBottom = 23 # 페이지 내 세번째 시작 행
@@ -20,122 +25,142 @@ class Main:
     location_Col_2 = "U" # 번호확인 함수 위치
     location_Col_3 = "O" # 설명확인 함수 위치
     imageCycle = 1   # 각 동의 폴더에서 넣을 사진의 순서
-    contentCycle =  1 # 조사표 내용이 삽입되는 순서
+    contentCycle = 1 # 조사표 내용이 삽입되는 순서
     fileList = None
     fileListJpg = None
-    imageNum = 0  # .jpg로 끝나는 사진의 개수
-    pageNum = 0 # 사진이 무조건 다 들어가는 페이지수
-    lastPageNum = 0 # 마지막 페이지에서 모자란 사진 숫자
 
-    def setFileName(self, fileName):
-        self.fileName = fileName
-    def getFileName(self):
-        return self.fileName
-  
-    def setSheetsName(self, sheetsName):
-        self.sheetsName = sheetsName
-    def getSheetsName(self):
-        return self.sheetsName
-    
-    def setBuildingName(self, buildingName):
-        self.buildingName = buildingName
-    def getBuildingName(self):
-        return self.buildingName
 
-    def setPath(self):
-        self.path = os.getcwd()
-    def getPath(self):
-        return self.path
-    
-    def setExcel(self):
-        self.excel = win32.gencache.EnsureDispatch('Excel.Application')
-    def getExcel(self):
-        return self.excel
+    def setExcelFilePath(self, excelFilePath):
+        self.excelFilePath = excelFilePath
+    def getExcelFilePath(self):
+        return self.excelFilePath
 
-    def setWorkBook(self):
-        self.wb = self.excel.Workbooks.Open(self.path + "\\" + self.fileName + ".xlsx")
-    def getWorkBook(self):
-        return self.wb
-    
-    def setWorkSheets(self):
-        self.ws = self.wb.Sheets(self.sheetsName)
-    def getWorkSheets(self):
-        return self.ws
+    def setSheetsNameList(self, sheetName):
+        self.sheetsNameList.append(sheetName)
+    def getSheetsNameList(self):
+        return self.sheetsNameList
 
-    def setFileList(self):
-        self.fileList = os.listdir(self.path + "\\" + str(self.buildingName))
-    def getFileList(self):
-        return self.fileList
-    
-    def setFileListJpg(self):
-        self.fileListJpg = [file for file in self.fileList if file.endswith(".jpg") or file.endswith("JPG")]   
-    def getFileListJpg(self):
-        return self.fileListJpg
-    
+    def setImgFilePathList(self, imgFilePath):
+        self.imgFilePathList.append(imgFilePath)
+    def getImgFilePathList(self):
+        return self.imgFilePathList
+
+    def setImgFileList(self):
+        for index, filePath in enumerate(self.getImgFilePathList()):
+            tmpList = []
+            tmpList = os.listdir(filePath)
+            self.imgFileList.append([])
+            for tmp in tmpList:
+                if (tmp.endswith(".jpg") or tmp.endswith(".JPG")):
+                    self.imgFileList[index].append(tmp)
+              
+    def getImgFileList(self):
+        return self.imgFileList
+
     def setImageNum(self):
-        self.imageNum = len(self.fileListJpg)  # .jpg로 끝나는 사진의 개수
+        for size in self.getImgFileList():
+            self.imageNum.append(len(size))
+
     def getImageNum(self):
         return self.imageNum
     
     def setPageNum(self):
-        self.pageNum = self.imageNum // 3 # 사진이 무조건 다 들어가는 페이지수
+        for size in self.getImageNum():
+            self.pageNum.append(size // 3) # 사진이 무조건 다 들어가는 페이지수
     def getPageNum(self):
         return self.pageNum
     
     def setLastPageNum(self):
-        self.lastPageNum = 3 - (self.imageNum % 3) # 마지막 페이지에서 모자란 사진 숫자
+        for size in self.getImageNum():
+            self.lastPageNum.append(3 - (size % 3))
+ # 마지막 페이지에서 모자란 사진 숫자
     def getLastPageNum(self):
         return self.lastPageNum
+    
 
+    def setWorkBook(self):
 
-    # 이미지를 복사해주는 함수    
+        self.wb = self.excel.Workbooks.Open(self.getExcelFilePath())
+    def getWorkBook(self):
+        return self.wb
+
+    def setWorkSheets(self):
+        for index in self.getSheetsNameList():
+            if self.wb.Sheets(index):
+                self.ws.append(self.wb.Sheets(index))
+        
+
+    def getWorkSheets(self):
+        return self.ws
+
+    def setWorkSheetsName(self):
+        # self.ws = self.wb.Sheets(self.sheetsName)
+        self.wsName = [sheet.Name for sheet in self.wb.Sheets]
+        
+    def getWorkSheetsName(self):
+        return self.wsName
+
+# 이미지를 복사해주는 함수    
     def duplicateImage(self):
-        if(self.imageNum % 3 > 0):
-            self.pageNum = self.pageNum +1 # 다들어가지 않고 남는 사진이 있으면 남는 사진이 들어간는 페이지 수 추가
-        
-        
-            #사진 갯수가 3의 배수가 아니면 마지막 사진을 그다음번호로 복사함 
-            for k in range(1, self.lastPageNum+1, 1):
-                shutil.copyfile(self.path + "\\" + str(self.buildingName) + "\\" + str(self.imageNum) + ".jpg", self.path + "\\" + str(self.buildingName) + "\\" + str(self.imageNum+k) + ".jpg") 
-
+        for index, size in enumerate(self.getLastPageNum()):
+            if ((size < 3) and (size > 0)):
+                self.pageNum[index] += 1
+                for k in range(1, self.lastPageNum[index]+1, 1):
+                    shutil.copyfile(self.getImgFilePathList()[index] +  "\\" + str(self.imageNum[index]) + ".jpg",self.getImgFilePathList()[index] +  "\\" + str(self.imageNum[index]+1) + ".jpg")
+                    self.imgFileList[index].append(str(self.imageNum[index]+1) + ".jpg")
+           
+    
+    # def duplicateImage(self):
+    #     try:
+    #         for index, size in enumerate(self.getLastPageNum()):
+    #             if ((size < 3) and (size > 0)):
+    #                 self.pageNum[index] += 1
+    #                 for k in range(1, self.lastPageNum[index]+1, 1):
+    #                     shutil.copyfile(self.getImgFilePathList()[index] +  "\\" + str(self.imageNum[index]) + ".jpg",self.getImgFilePathList()[index] +  "\\" + str(self.imageNum[index]+1) + ".jpg")
+    #                     self.imgFileList[index].append(str(self.imageNum[index]+1) + ".jpg")
+    #                     self.imageNum[index] += 1
+    #         return None
+    #     except Exception as e:
+    #         errorMessage = "에러 발생", str(self.getWorkSheets()[index]) + "에 들어갈 " + str(self.imageNum[index]) + ".jpg 사진이 없습니다."
+    #         return errorMessage
 
     # 화살표를 복사해주는 함수
-    def inputArrow(self, location_Row):
-        self.ws.Range("Z1:AF4").Copy(self.ws.Range("U"+str(location_Row)))
+    def inputArrow(self, location_Row, index):
+        self.ws[index].Range("Z1:AF4").Copy(self.ws[index].Range("U"+str(location_Row)))
     
 
     # 결함의 위치를 삽입해주는 함수
-    def inputLocation(self, location_Row):
-        self.ws.Cells(int(location_Row)+1, 17).Value = (str(self.ws.Cells(int(self.contentCycle)+8, 33)))+"\n"+(str(self.ws.Cells(int(self.contentCycle)+8, 35)))
+    def inputLocation(self, location_Row, index):
+        self.ws[index].Cells(int(location_Row)+1, 17).Value = (str(self.ws[index].Cells(int(self.contentCycle)+8, 33)))+"\n"+(str(self.ws[index].Cells(int(self.contentCycle)+8, 35)))
         return ()
 
 
     # 조사표 내용을 조사사진에 넣어주는 함수
-    def inputContents(self,location_Row):
+    def inputContents(self,location_Row, index):
         #41,43,45,47,49    25,38
-        self.ws.Cells(int(location_Row)+8, 15).Value = self.ws.Cells(int(self.contentCycle)+9, 41)
-        self.ws.Cells(int(location_Row)+8, 17).Value = self.ws.Cells(int(self.contentCycle)+9, 43)
-        self.ws.Cells(int(location_Row)+8, 19).Value = self.ws.Cells(int(self.contentCycle)+9, 45)
-        self.ws.Cells(int(location_Row)+8, 21).Value = self.ws.Cells(int(self.contentCycle)+9, 47)
-        self.ws.Cells(int(location_Row)+8, 23).Value = self.ws.Cells(int(self.contentCycle)+9, 49)
-        self.ws.Cells(int(location_Row)+8, 25).Value = self.ws.Cells(int(self.contentCycle)+9, 38)
+        self.ws[index].Cells(int(location_Row)+8, 15).Value = self.ws[index].Cells(int(self.contentCycle)+9, 41)
+        self.ws[index].Cells(int(location_Row)+8, 17).Value = self.ws[index].Cells(int(self.contentCycle)+9, 43)
+        self.ws[index].Cells(int(location_Row)+8, 19).Value = self.ws[index].Cells(int(self.contentCycle)+9, 45)
+        self.ws[index].Cells(int(location_Row)+8, 21).Value = self.ws[index].Cells(int(self.contentCycle)+9, 47)
+        self.ws[index].Cells(int(location_Row)+8, 23).Value = self.ws[index].Cells(int(self.contentCycle)+9, 49)
+        self.ws[index].Cells(int(location_Row)+8, 25).Value = self.ws[index].Cells(int(self.contentCycle)+9, 38)
         self.contentCycle = self.contentCycle + 1
         return(self.contentCycle)
 
 
     # 설명번호와 사진번호가 맞는지 확인해주는 엑셀수식을 삽입해주는 함수
-    def checkImageNum(self, location_Row):
-        self.ws.Cells(int(location_Row)+1, 21).Value = "=IF(MID(O"+str(int(location_Row)+1)+",SEARCH(\".\",O"+str(int(location_Row)+1)+",1),3)=MID(W"+str(int(location_Row)+8)+",SEARCH(\".\",W"+str(int(location_Row)+8)+",1),3),\"\",\"번호확인\")" 
-        self.ws.Cells(int(location_Row)+1, 21).Font.Color = -16776961
+    def checkImageNum(self, location_Row, index):
+        self.ws[index].Cells(int(location_Row)+1, 21).Value = "=IF(MID(O"+str(int(location_Row)+1)+",SEARCH(\".\",O"+str(int(location_Row)+1)+",1),3)=MID(W"+str(int(location_Row)+8)+",SEARCH(\".\",W"+str(int(location_Row)+8)+",1),3),\"\",\"번호확인\")" 
+        self.ws[index].Cells(int(location_Row)+1, 21).Font.Color = -16776961
         return()
 
 
     # 설명부분 내용을 합성해주는 엑셀수식을 삽입해주는 함수
-    def combineExplanation(self, location_Row, location_Col):
-        if(self.ws.Cells(int(location_Row)+8, 25).Value == "균열"):
-            self.ws.Cells(int(location_Row)+4, 15).Value = "="+ location_Col + str(int(location_Row) + 8) + "&"+"\"균열\"" 
+    def combineExplanation(self, location_Row, location_Col, index):
+        if(self.ws[index].Cells(int(location_Row)+8, 25).Value == "균열"):
+            self.ws[index].Cells(int(location_Row)+4, 15).Value = "="+ location_Col + str(int(location_Row) + 8) + "&"+"\"균열\"" 
         else:
-            self.ws.Cells(int(location_Row)+4, 15).Value = self.ws.Cells(int(location_Row)+8, 25)
+            self.ws[index].Cells(int(location_Row)+4, 15).Value = self.ws[index].Cells(int(location_Row)+8, 25)
         return()
 
 
@@ -146,11 +171,11 @@ class Main:
 
 
     # 행, 열, 사진경로, 폴더이름, 사진 번호를 받고 사진을 해당위치에 삽입 및 다음 사진 넘버를 반환 해주는 함수
-    def inputImage(self, location_Row, location_Col):
+    def inputImage(self, location_Row, location_Col, index):
         location = location_Col + str(location_Row)
-        rng = self.ws.Range(location) 
-        ImagePath = self.path+"\\" + str(self.buildingName) + "\\" + str(self.imageCycle) + ".jpg" 
-        Image = self.ws.Shapes.AddPicture(ImagePath, False,True, rng.Left, rng.Top, 247.68, 184.28)
+        rng = self.ws[index].Range(location) 
+        ImagePath = self.getImgFilePathList()[index] + "/" + str(self.imageCycle) + ".jpg"
+        ImagePath = ImagePath.replace("/","\\")
+        self.ws[index].Shapes.AddPicture(ImagePath, False,True, rng.Left, rng.Top, 247.68, 184.28)
         self.imageCycle = self.imageCycle + 1
         return(self.imageCycle) 
-
